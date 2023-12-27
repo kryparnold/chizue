8ab2ec1b9fb2f22d55b83763790bc61560da0639
kryparnold <emirhanpgt@gmail.com>
2023-12-27 08:02:30 +0300

    misc & util changes

---
+++ src/client/Utils.ts
@@ -0,0 +1,13 @@
+export class Utils{
+    static Letters = {
+        tr: "abcçdefghıijklmnoöprsştuüvyz",
+        en: "abcdefghijklmnopqrstuvwxyz"
+    }
+    static random<T>(array: T[]){
+        return array[Math.floor(Math.random() * array.length)];
+    }
+    static avatarBaseURL = "https://cdn.discordapp.com/";
+    static getAvatarURL(userId: string,avatarHash: string){
+        return `${this.avatarBaseURL}avatars/${userId}/${avatarHash}.png?size=64`
+    }
+}
--- src/globals.ts
+++ src/globals.ts
@@ -1,4 +1,7 @@
 export * from "./types";
 export * from "./database/database";
+export * from "./client/Utils";
+export * from "./client/Localizations";
 export * from "./managers/Logger";
+export * from "./managers/Words";
 export * from "./client/Client";
