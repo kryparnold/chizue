7d5e745bb4be4b3764c23261104ccf0002430e1f
kryparnold <emirhanpgt@gmail.com>
2023-12-31 18:24:11 +0300

    added check for existing another game on channel and misc changes

--- src/commands/word-game.ts
+++ src/commands/word-game.ts
@@ -15,7 +15,7 @@ import {
 	StringSelectMenuOptionBuilder,
 } from "discord.js";
 import { Utils, prisma, client } from "@/globals";
-import { GameMode, Locales } from "@prisma/client";
+import { GameMode, GameType, Locales } from "@prisma/client";
 
 // Export a default object with data and execute properties
 export default {
@@ -24,8 +24,8 @@ export default {
 		.setName("word-game")
 		// Localization support for name and description
 		.setNameLocalization("tr", "kelime-oyunu")
-		.setDescription("You can setup, delete or modify Word Game with this command.")
-		.setDescriptionLocalization("tr", "Bu komutla Kelime Oyunu'nu kurabilir, kaldırabilir ya da düzenleyebilirsin.")
+		.setDescription("You can setup, delete or modify Word Games with this command.")
+		.setDescriptionLocalization("tr", "Bu komutla Kelime Oyunu kurabilir, kaldırabilir ya da düzenleyebilirsin.")
 		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
 		.setDMPermission(false)
 		// Add a required channel option
@@ -85,12 +85,12 @@ export default {
 		const selectLocale = new StringSelectMenuBuilder().setCustomId("_locale").addOptions(
 			// Options for different locales
 			new StringSelectMenuOptionBuilder()
-				.setLabel(client.getLocalization(userLocale, "channelLocaleTr"))
-				.setDescription(client.getLocalization(userLocale, "channelLocaleTrDesc"))
+				.setLabel(client.getLocalization(userLocale, "gameLocaleTr"))
+				.setDescription(client.getLocalization(userLocale, "gameLocaleTrDesc"))
 				.setValue("Turkish"),
 			new StringSelectMenuOptionBuilder()
-				.setLabel(client.getLocalization(userLocale, "channelLocaleEn"))
-				.setDescription(client.getLocalization(userLocale, "channelLocaleEnDesc"))
+				.setLabel(client.getLocalization(userLocale, "gameLocaleEn"))
+				.setDescription(client.getLocalization(userLocale, "gameLocaleEnDesc"))
 				.setValue("English")
 				.setDefault(true)
 		);
@@ -109,6 +109,12 @@ export default {
 
 		// Update components based on existing channel data
 		if (channel) {
+
+            if(channel.type !== GameType.WordGame){
+                await interaction.editReply(client.getLocalization<true>(userLocale,"gameAnother")(client.getLocalization(userLocale,channel.type)));
+                return;
+            }
+
 			// Update embed description and fields
 			embed.data.description = client.getLocalization<true>(userLocale, "gameExists")(client.getLocalization(userLocale, channel.type));
 			embed.data.fields = await this.getFields(userLocale, {
@@ -154,225 +160,225 @@ export default {
 		// Create a message component collector for button and select menu interactions
 		const componentCollector = reply.createMessageComponentCollector({ time: 90000 });
 
-		// Handle interactions within the collector
-		componentCollector.on("collect", async (componentInteraction) => {
-			const customId = componentInteraction.customId;
-			await componentInteraction.deferReply({ ephemeral: true });
-			if (componentInteraction.isStringSelectMenu()) {
-				// Handle interactions for string select menus
-				const selectedOption: any = componentInteraction.values[0];
+		componentCollector
+			// Handle interactions within the collector
+			.on("collect", async (componentInteraction) => {
+				const customId = componentInteraction.customId;
+				await componentInteraction.deferReply({ ephemeral: true });
+				if (componentInteraction.isStringSelectMenu()) {
+					// Handle interactions for string select menus
+					const selectedOption: any = componentInteraction.values[0];
 
-				const options = componentInteraction.component.options;
+					const options = componentInteraction.component.options;
 
-				const localizedOption = options.find((option) => option.value === selectedOption)?.label as string;
+					const localizedOption = options.find((option) => option.value === selectedOption)?.label as string;
 
-				if (customId.endsWith("locale")) {
-					// Handle locale change
-					channelPreferences.locale = selectedOption;
-					await componentInteraction.editReply({
-						content: client.getLocalization<true>(userLocale, "gameLangChange")(localizedOption),
-					});
-				} else if (customId.endsWith("mode")) {
-					// Handle game mode change
-					channelPreferences.mode = selectedOption;
-					await componentInteraction.editReply({
-						content: client.getLocalization<true>(userLocale, "gameModeChange")(localizedOption),
-					});
-				}
+					if (customId.endsWith("locale")) {
+						// Handle locale change
+						channelPreferences.locale = selectedOption;
+						await componentInteraction.editReply({
+							content: client.getLocalization<true>(userLocale, "gameLangChange")(localizedOption),
+						});
+					} else if (customId.endsWith("mode")) {
+						// Handle game mode change
+						channelPreferences.mode = selectedOption;
+						await componentInteraction.editReply({
+							content: client.getLocalization<true>(userLocale, "gameModeChange")(localizedOption),
+						});
+					}
 
-				// Update channel data in the database if it exists
-				if (channel) {
-					const newChannel = await prisma.wordGame.update({
-						where: {
-							id: channel.id,
-						},
-						data: {
-							mode: channelPreferences.mode,
-							locale: channelPreferences.locale,
-						},
-					});
-
-					// Update channel and components if the database update is successful
-					if (newChannel) {
-						channel = newChannel;
-
-						selectLocale.options.forEach((item) => {
-							if (item.data.value === channel?.locale) {
-								item.setDefault(true);
-							} else {
-								item.setDefault(false);
-							}
+					// Update channel data in the database if it exists
+					if (channel) {
+						const newChannel = await prisma.wordGame.update({
+							where: {
+								id: channel.id,
+							},
+							data: {
+								mode: channelPreferences.mode,
+								locale: channelPreferences.locale,
+							},
 						});
 
-						selectMode.options.forEach((item) => {
-							if (item.data.value === channel?.mode) {
-								item.setDefault(true);
-							} else {
-								item.setDefault(false);
-							}
+						// Update channel and components if the database update is successful
+						if (newChannel) {
+							channel = newChannel;
+
+							selectLocale.options.forEach((item) => {
+								if (item.data.value === channel?.locale) {
+									item.setDefault(true);
+								} else {
+									item.setDefault(false);
+								}
+							});
+
+							selectMode.options.forEach((item) => {
+								if (item.data.value === channel?.mode) {
+									item.setDefault(true);
+								} else {
+									item.setDefault(false);
+								}
+							});
+
+							await interaction.editReply({
+								embeds: [
+									embed.setFields(
+										await this.getFields(userLocale, {
+											channelLocale: channel.locale,
+											mode: channel.mode,
+											wordCount: channel.words.length,
+										})
+									),
+								],
+								components: rows,
+							});
+						}
+					}
+				} else if (componentInteraction.isButton()) {
+					// Handle interactions for buttons
+					if (customId.endsWith("setup")) {
+						// Handle setup button click
+						const randomLetter = Utils.random(Array.from(Utils.Letters["en"]));
+
+						const newChannel = await prisma.wordGame.create({
+							data: {
+								id: selectedChannel.id,
+								letter: randomLetter,
+								locale: channelPreferences.locale,
+								mode: channelPreferences.mode,
+							},
 						});
 
-						await interaction.editReply({
-							embeds: [
-								embed.setFields(
+						// Update channel and components if channel creation is successful
+						if (newChannel) {
+							channel = newChannel;
+
+							embed
+								.setDescription(client.getLocalization<true>(userLocale, "gameExists")(client.getLocalization(userLocale, channel.type)))
+								.addFields(
 									await this.getFields(userLocale, {
 										channelLocale: channel.locale,
 										mode: channel.mode,
 										wordCount: channel.words.length,
 									})
-								),
-							],
-							components: rows,
-						});
-					}
-				}
-			} else if (componentInteraction.isButton()) {
-				// Handle interactions for buttons
-				if (customId.endsWith("setup")) {
-					// Handle setup button click
-					const randomLetter = Utils.random(Array.from(Utils.Letters["en"]));
+								);
 
-					const newChannel = await prisma.wordGame.create({
-						data: {
-							id: selectedChannel.id,
-							letter: randomLetter,
-							locale: channelPreferences.locale,
-							mode: channelPreferences.mode,
-						},
-					});
-
-					// Update channel and components if channel creation is successful
-					if (newChannel) {
-						channel = newChannel;
-
-						embed
-							.setDescription(client.getLocalization<true>(userLocale, "gameExists")(client.getLocalization(userLocale, channel.type)))
-							.addFields(
-								await this.getFields(userLocale, {
-									channelLocale: channel.locale,
-									mode: channel.mode,
-									wordCount: channel.words.length,
-								})
-							);
-
-						selectLocale.options.forEach((item) => {
-							if (item.data.value === channel?.locale) {
-								item.setDefault(true);
-							} else {
-								item.setDefault(false);
-							}
-						});
-
-						selectMode.options.forEach((item) => {
-							if (item.data.value === channel?.mode) {
-								item.setDefault(true);
-							} else {
-								item.setDefault(false);
-							}
-						});
-
-						await interaction.editReply({
-							embeds: [embed],
-							components: rows.map((item, index) => {
-								if (index === rows.length - 1) {
-									item.components[0].setDisabled(true);
-									item.components[1].setDisabled(false);
+							selectLocale.options.forEach((item) => {
+								if (item.data.value === channel?.locale) {
+									item.setDefault(true);
+								} else {
+									item.setDefault(false);
 								}
-
-								return item;
-							}),
-						});
-						await componentInteraction.editReply({
-							content: client.getLocalization(userLocale, "gameSetupSuccess"),
-						});
-					} else {
-						await componentInteraction.editReply({
-							content: client.getLocalization(userLocale, "anErrorOccured"),
-						});
-					}
-				} else if (customId.endsWith("remove")) {
-					// Handle remove button click
-					const confirmEmbed = new EmbedBuilder()
-						.setTitle(client.getLocalization(userLocale, "confirmRemove"))
-						.setDescription(client.getLocalization(userLocale, "confirmRemoveDesc"))
-						.setColor(Colors.Red)
-						.setFooter(userFooter);
-
-					const confirmButton = new ButtonBuilder()
-						.setCustomId("_confirm")
-						.setLabel(client.getLocalization(userLocale, "buttonConfirm"))
-						.setStyle(ButtonStyle.Primary);
-
-					const row = new ActionRowBuilder<ButtonBuilder>().addComponents(confirmButton);
-
-					// Display a confirmation message and button
-					const componentReply = await componentInteraction.editReply({
-						embeds: [confirmEmbed],
-						components: [row],
-					});
-					// Await user confirmation or timeout
-					componentReply
-						.awaitMessageComponent({ time: 15000, componentType: ComponentType.Button })
-						.then(async (buttonInteraction) => {
-							await buttonInteraction.deferReply({ ephemeral: true });
-
-							// Delete the Word Game from the database upon confirmation
-							const oldGame = await prisma.wordGame.delete({
-								where: {
-									id: selectedChannel.id,
-								},
 							});
 
-							// Update embed and components after successful removal
-							if (oldGame) {
-								embed.data.description = client.getLocalization(userLocale, "gameDoesntExists");
-								embed.data.fields = [];
+							selectMode.options.forEach((item) => {
+								if (item.data.value === channel?.mode) {
+									item.setDefault(true);
+								} else {
+									item.setDefault(false);
+								}
+							});
 
-								interaction.editReply({
-									embeds: [embed],
-									components: rows.map((item, index) => {
-										if (index === rows.length - 1) {
-											item.components[1].setDisabled(true);
-											item.components[0].setDisabled(false);
-										}
+							await interaction.editReply({
+								embeds: [embed],
+								components: rows.map((item, index) => {
+									if (index === rows.length - 1) {
+										item.components[0].setDisabled(true);
+										item.components[1].setDisabled(false);
+									}
 
-										return item;
-									}),
+									return item;
+								}),
+							});
+							await componentInteraction.editReply({
+								content: client.getLocalization(userLocale, "gameSetupSuccess"),
+							});
+						} else {
+							await componentInteraction.editReply({
+								content: client.getLocalization(userLocale, "anErrorOccured"),
+							});
+						}
+					} else if (customId.endsWith("remove")) {
+						// Handle remove button click
+						const confirmEmbed = new EmbedBuilder()
+							.setTitle(client.getLocalization(userLocale, "confirmRemove"))
+							.setDescription(client.getLocalization(userLocale, "confirmRemoveDesc"))
+							.setColor(Colors.Red)
+							.setFooter(userFooter);
+
+						const confirmButton = new ButtonBuilder()
+							.setCustomId("_confirm")
+							.setLabel(client.getLocalization(userLocale, "buttonConfirm"))
+							.setStyle(ButtonStyle.Primary);
+
+						const row = new ActionRowBuilder<ButtonBuilder>().addComponents(confirmButton);
+
+						// Display a confirmation message and button
+						const componentReply = await componentInteraction.editReply({
+							embeds: [confirmEmbed],
+							components: [row],
+						});
+						// Await user confirmation or timeout
+						componentReply
+							.awaitMessageComponent({ time: 15000, componentType: ComponentType.Button })
+							.then(async (buttonInteraction) => {
+								await buttonInteraction.deferReply({ ephemeral: true });
+
+								// Delete the Word Game from the database upon confirmation
+								const oldGame = await prisma.wordGame.delete({
+									where: {
+										id: selectedChannel.id,
+									},
 								});
 
-								buttonInteraction.editReply({
-									content: client.getLocalization(userLocale, "gameRemoveSuccess"),
-								});
+								// Update embed and components after successful removal
+								if (oldGame) {
+									embed.data.description = client.getLocalization(userLocale, "gameDoesntExists");
+									embed.data.fields = [];
 
+									interaction.editReply({
+										embeds: [embed],
+										components: rows.map((item, index) => {
+											if (index === rows.length - 1) {
+												item.components[1].setDisabled(true);
+												item.components[0].setDisabled(false);
+											}
+
+											return item;
+										}),
+									});
+
+									buttonInteraction.editReply({
+										content: client.getLocalization(userLocale, "gameRemoveSuccess"),
+									});
+
+									componentInteraction.editReply({
+										embeds: [confirmEmbed.setDescription(client.getLocalization(userLocale, "confirmUsed"))],
+										components: [],
+									});
+								} else {
+									buttonInteraction.editReply({
+										embeds: [confirmEmbed.setDescription(client.getLocalization(userLocale, "anErrorOccured"))],
+									});
+								}
+							})
+							.catch(() => {
 								componentInteraction.editReply({
-                                    embeds: [confirmEmbed.setDescription(client.getLocalization(userLocale, "confirmUsed"))],
+									embeds: [confirmEmbed.setDescription(client.getLocalization(userLocale, "confirmRemoveExpire"))],
 									components: [],
 								});
-
-							} else {
-								buttonInteraction.editReply({
-									embeds: [confirmEmbed.setDescription(client.getLocalization(userLocale, "anErrorOccured"))],
-								});
-							}
-						})
-						.catch(() => {
-							componentInteraction.editReply({
-								embeds: [confirmEmbed.setDescription(client.getLocalization(userLocale, "confirmRemoveExpire"))],
-								components: [],
 							});
-						});
+					}
 				}
-			}
-		});
+			})
 
-		// Handle the end of the collector by updating the embed and components
-		componentCollector.on("end", async () => {
-			embed.setDescription(client.getLocalization<true>(userLocale, "commandTimeout")(this.data.name)).setFields([]);
-			await interaction.editReply({
-				embeds: [embed],
-				components: [],
+			// Handle the end of the collector by updating the embed and components
+			.on("end", async () => {
+				embed.setDescription(client.getLocalization<true>(userLocale, "commandTimeout")(this.data.name)).setFields([]);
+				await interaction.editReply({
+					embeds: [embed],
+					components: [],
+				});
 			});
-		});
 	},
 	// Helper function to generate fields for the embed
 	async getFields(locale: Locale, props: { wordCount: number; channelLocale: Locales; mode: GameMode }) {
@@ -380,11 +386,11 @@ export default {
 
 		return [
 			{
-				name: client.getLocalization(locale, "channelWordCount"),
+				name: client.getLocalization(locale, "gameWordCounter"),
 				value: wordCount.toString(),
 			},
 			{
-				name: client.getLocalization(locale, "channelLocale"),
+				name: client.getLocalization(locale, "gameLocale"),
 				value: client.getLocalization(locale, channelLocale),
 			},
 			{
