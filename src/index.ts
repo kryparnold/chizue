e8e85bd490cf34dadb5bfabebd944a0cc1437c50
kryparnold <emirhanpgt@gmail.com>
2023-12-31 08:45:51 +0300

    added comments

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,48 @@
+// Importing necessary modules and components from external files
 import { Events } from "discord.js";
-import { BotStatuses, client } from "./globals";
+import { BotStatuses, Utils, client } from "./globals";
+
+// Importing and configuring environment variables using dotenv
 import { config as envConfig } from "dotenv";
 envConfig();
 
+// Event handler for when the Discord client is ready
 client.once(Events.ClientReady, async (readyClient) => {
-    console.log(readyClient.user.username + " is online.");
+	// Recording the start time for measuring initialization duration
+	const startTime = new Date().getTime();
 
-    const startTime = new Date().getTime();
-    await client.init();
-    client.logger.log(
-        `Client initialization completed in **${
-            new Date().getTime() - startTime
-        }ms**.`
-    );
+	// Initializing the client and waiting for completion
+	await client.init();
+
+	// Logging the completion of client initialization with the duration
+	client.logger.log(`Client initialization completed in **${new Date().getTime() - startTime}ms**.`);
 });
 
+// Event handler for when a message is created in a channel
+client.on(Events.MessageCreate, async (message) => {
+	if (client.status === BotStatuses.Initializing) return; //Check if the client is still initializing
+
+	// Process the message by sending it to the client for handling games
+	await client.handleMessage(message);
+});
+
+// Event handler for when an interaction (command or button) is created
 client.on(Events.InteractionCreate, async (interaction) => {
-    if (client.status === BotStatuses.INITIALIZING) return;
+	// Check if the client is still initializing, and if so, return early
+	if (client.status === BotStatuses.Initializing) return;
 
-    if(interaction.isChatInputCommand()){
-        await client.handleCommand(interaction);
-    }else if(interaction.isButton()){
-        await client.handleButton(interaction);
-    }
+	// Check if the interaction is a chat input command
+	if (interaction.isChatInputCommand()) {
+		// Handle the command
+		await client.handleCommand(interaction);
+	} else if (interaction.isButton()) {
+		// Handle the button interaction
+		await client.handleButton(interaction);
+	}
 });
 
+// Log in to Discord using the provided bot token
 client.login(process.env.TOKEN);
 
+// Export the client for external use if needed
 export { client };
