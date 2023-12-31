f9194ac09eb723948d26e6b8f48577f4d75bb2ab
kryparnold <emirhanpgt@gmail.com>
2023-12-31 17:25:40 +0300

    removed redundant if and splitted initializations

--- src/classes/Games.ts
+++ src/classes/Games.ts
@@ -5,13 +5,18 @@ import { CountingGame, WordGame } from "@/globals";
 export class Games {
 	private cache = new Collection<string, WordGame | CountingGame>();
 
-	async init(games: (WordGameModel | CountingGameModel)[]) {
-		games.forEach(async (game) => {
+	async init(wordGames: WordGameModel[],countingGames: CountingGameModel[]) {
+        wordGames.forEach(async (game) => {
+            const newGame = new WordGame(game);
 
-			const newGame = game.type === GameType.WordGame ? new WordGame(game as WordGameModel) : new CountingGame(game as CountingGameModel);
+            this.cache.set(game.id, newGame);
+        });
 
-			this.cache.set(game.id, newGame);
-		});
+        countingGames.forEach(async (game) => {
+            const newGame = new CountingGame(game);
+
+            this.cache.set(game.id, newGame);
+        });
 	}
 
 	get(gameId: string) {
