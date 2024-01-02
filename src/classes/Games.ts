6427e48636fac232f4db7976566dbc42fd11c12a
kryparnold <emirhanpgt@gmail.com>
2024-01-02 21:34:20 +0300

    whitespace

--- src/classes/Games.ts
+++ src/classes/Games.ts
@@ -6,60 +6,60 @@ export class Games {
 	private cache = new Collection<string, WordGame | CountingGame>();
 
 	async init(wordGames: RawWordGame[], rawPlayers: RawPlayer[], countingGames: RawCountingGame[]) {
-        wordGames.forEach(async (game) => {
-            const players = new Players();
-            await players.init(rawPlayers.filter((player) => player.wordGameId === game.id));
-            const newGame = new WordGame({ ...game, players });
-            
-            this.cache.set(game.id, newGame);
-        });
+		wordGames.forEach(async (game) => {
+			const players = new Players();
+			await players.init(rawPlayers.filter((player) => player.wordGameId === game.id));
+			const newGame = new WordGame({ ...game, players });
 
-        countingGames.forEach(async (game) => {
-            const newGame = new CountingGame(game);
+			this.cache.set(game.id, newGame);
+		});
 
-            this.cache.set(game.id, newGame);
-        });
+		countingGames.forEach(async (game) => {
+			const newGame = new CountingGame(game);
+
+			this.cache.set(game.id, newGame);
+		});
 	}
 
-async createWordGame(game: Prisma.WordGameCreateInput) {
+	async createWordGame(game: Prisma.WordGameCreateInput) {
 		const newRawGame = await prisma.wordGame.create({
-            data: game
-        });
+			data: game,
+		});
 
-        const newGame = new WordGame({...newRawGame,players: new Players()});
+		const newGame = new WordGame({ ...newRawGame, players: new Players() });
 
-        this.cache.set(newGame.id,newGame);
+		this.cache.set(newGame.id, newGame);
 
-        return newGame;
+		return newGame;
 	}
 
 	async createCountingGame(game: Prisma.CountingGameCreateInput) {
 		const newRawGame = await prisma.countingGame.create({
-            data: game
-        });
+			data: game,
+		});
 
-        const newGame = new CountingGame(newRawGame);
+		const newGame = new CountingGame(newRawGame);
 
-        this.cache.set(newGame.id,newGame);
+		this.cache.set(newGame.id, newGame);
 
-        return newGame;
+		return newGame;
 	}
 
-	async delete(gameId: string) {
-        const game = this.cache.get(gameId);
+	async delete(gameId = "") {
+		const game = this.cache.get(gameId);
 
-        if(!game){
-            throw "Game not found with id: " + gameId;
-        }
+		if (!game) {
+			throw "Game not found with id: " + gameId;
+		}
 
-        if(game?.type === GameType.WordGame){
-            await prisma.wordGame.delete({where: {id: gameId}});
-        }else if(game?.type === GameType.CountingGame){
-            await prisma.countingGame.delete({where: {id: gameId}});
-        }
+		if (game?.type === GameType.WordGame) {
+			await prisma.wordGame.delete({ where: { id: gameId } });
+		} else if (game?.type === GameType.CountingGame) {
+			await prisma.countingGame.delete({ where: { id: gameId } });
+		}
 
-        this.cache.delete(gameId);
-    }
+		this.cache.delete(gameId);
+	}
 
 	get(gameId: string) {
 		return this.cache.get(gameId);
