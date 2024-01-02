07806c575d8e37d75e9cc2679dcb8e07e02b0007
kryparnold <emirhanpgt@gmail.com>
2024-01-02 14:12:32 +0300

    added unique key to player

--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -2,29 +2,32 @@ import { prisma } from "@/globals";
 import { Player as PlayerModel } from "@prisma/client";
 
 export class Player {
-    id: string;
-    score: number;
-    wordGameId: string | null;
+	key: string;
+	id: string;
+	score: number;
+	wordGameId: string | null;
 
-    constructor(player: PlayerModel){
-        this.id = player.id;
-        this.score = player.score;
-        this.wordGameId = player.wordGameId;
-    }
+	constructor(player: PlayerModel) {
+		this.key = player.key;
+		this.id = player.id;
+		this.score = player.score;
+		this.wordGameId = player.wordGameId;
+	}
 
-    async addScore(score: number){
-        this.score += score;
-        await this.save();
-    }
+	async addScore(score: number) {
+		this.score += score;
+		this.score = +this.score.toFixed(1);
+		await this.save();
+	}
 
-    async save(){
-        await prisma.player.update({
-            where: {
-                id: this.id
-            },
-            data: {
-                score: this.score
-            }
-        })
-    }
+	async save() {
+		await prisma.player.update({
+			where: {
+				key: this.key,
+			},
+			data: {
+				score: this.score,
+			},
+		});
+	}
 }
