a145a4f18220e7c19ca6ff1905701f2ad26bfdd9
kryparnold <emirhanpgt@gmail.com>
2024-01-05 22:43:00 +0300

    added comments and format changes

--- src/classes/Games.ts
+++ src/classes/Games.ts
@@ -1,76 +1,106 @@
+// Import necessary modules and types from Prisma, Discord.js, and local files
 import { WordGame as RawWordGame, CountingGame as RawCountingGame, Player as RawPlayer, GameType, Prisma } from "@prisma/client";
 import { Collection, TextChannel } from "discord.js";
 import { CountingGame, Players, Utils, WordGame, client, prisma } from "@/globals";
 
+// Define a class called Games to manage and interact with WordGame and CountingGame instances
 export class Games {
+	// Private property to store a cache of WordGame and CountingGame instances
 	private cache = new Collection<string, WordGame | CountingGame>();
 
+	// Initialization method, takes arrays of raw WordGame, Player, and CountingGame data
 	async init(wordGames: RawWordGame[], rawPlayers: RawPlayer[], countingGames: RawCountingGame[]) {
+		// Initialize WordGame instances
 		wordGames.forEach(async (game) => {
+			// Create Players instance and initialize it with filtered player data
 			const players = new Players();
 			await players.init(rawPlayers.filter((player) => player.wordGameId === game.id));
+			// Create new WordGame instance and add it to the cache
 			const newGame = new WordGame({ ...game, players });
 
 			this.cache.set(game.id, newGame);
 		});
 
+		// Initialize CountingGame instances
 		countingGames.forEach(async (game) => {
+			// Create new CountingGame instance and add it to the cache
 			const newGame = new CountingGame(game);
 
 			this.cache.set(game.id, newGame);
 		});
 	}
 
+	// Method to create a new WordGame and add it to the cache
 	async createWordGame(game: Prisma.WordGameCreateInput) {
+		// Create a new WordGame in the database using Prisma
 		const newRawGame = await prisma.wordGame.create({
 			data: game,
 		});
 
+		// Create a new WordGame instance with an empty Players instance
 		const newGame = new WordGame({ ...newRawGame, players: new Players() });
 
+		// Add the new WordGame instance to the cache
 		this.cache.set(newGame.id, newGame);
 
-        const gameChannel = client.channels.cache.get(newGame.id) as TextChannel;
-        await gameChannel.send(client.getLocalization<true>(Utils.formatLocale(newGame.locale),"wordGameStarted")(newGame.letter));
+		// Get the game channel and send a message indicating the start of the WordGame
+		const gameChannel = client.channels.cache.get(newGame.id) as TextChannel;
+		await gameChannel.send(client.getLocalization<true>(Utils.formatLocale(newGame.locale), "wordGameStarted")(newGame.letter));
 
+		// Return the new WordGame instance
 		return newGame;
 	}
 
+	// Method to create a new CountingGame and add it to the cache
 	async createCountingGame(game: Prisma.CountingGameCreateInput) {
+		// Create a new CountingGame in the database using Prisma
 		const newRawGame = await prisma.countingGame.create({
 			data: game,
 		});
 
+		// Create a new CountingGame instance
 		const newGame = new CountingGame(newRawGame);
 
+		// Add the new CountingGame instance to the cache
 		this.cache.set(newGame.id, newGame);
 
-        const gameChannel = client.channels.cache.get(newGame.id) as TextChannel;
-        await gameChannel.send(client.getLocalization<true>(Utils.formatLocale(gameChannel.guild.preferredLocale),"countingGameStarted")(newGame.multiplier.toString()));
+		// Get the game channel and send a message indicating the start of the CountingGame
+		const gameChannel = client.channels.cache.get(newGame.id) as TextChannel;
+		await gameChannel.send(
+			client.getLocalization<true>(Utils.formatLocale(gameChannel.guild.preferredLocale), "countingGameStarted")(newGame.multiplier.toString())
+		);
 
+		// Return the new CountingGame instance
 		return newGame;
 	}
 
-    count() {
-        return this.cache.size;
-    }
+	// Method to get the total number of games in the cache
+	count() {
+		return this.cache.size;
+	}
 
+	// Method to delete a game from the cache and database
 	async delete(gameId = "") {
+		// Get the game instance from the cache
 		const game = this.cache.get(gameId);
 
+		// Check if the game instance exists
 		if (!game) {
 			throw "Game not found with id: " + gameId;
 		}
 
+		// Determine the game type and delete it from the database using Prisma
 		if (game?.type === GameType.WordGame) {
 			await prisma.wordGame.delete({ where: { id: gameId } });
 		} else if (game?.type === GameType.CountingGame) {
 			await prisma.countingGame.delete({ where: { id: gameId } });
 		}
 
+		// Remove the game instance from the cache
 		this.cache.delete(gameId);
 	}
 
+	// Method to get a game instance from the cache by its ID
 	get(gameId: string) {
 		return this.cache.get(gameId);
 	}
