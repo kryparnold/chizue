52b7f049345ef94a997914466cbcc0ef651191ae
kryparnold <emirhanpgt@gmail.com>
2024-01-05 22:43:14 +0300

    format changes and rename database/raw to json

--- src/classes/Words.ts
+++ src/classes/Words.ts
@@ -1,28 +1,24 @@
 import { IEnglishWords, ITurkishWords, IWordleWords } from "@/globals";
 
 export class Words {
-    tr!: ITurkishWords;
-    en!: IEnglishWords;
-    wordleWords!: IWordleWords;
+	tr!: ITurkishWords;
+	en!: IEnglishWords;
+	wordleWords!: IWordleWords;
 
-    async init(): Promise<{tr: number, en: number}> {
-        const turkishWords = (await import("@/database/raw/tr_words.json")).default;
-        const englishWords = (await import("@/database/raw/en_words.json")).default;
-        const wordleWords = (await import("@/database/raw/wordle_words.json")).default;
+	async init() {
+		const turkishWords = (await import("@/database/json/tr_words.json")).default;
+		const englishWords = (await import("@/database/json/en_words.json")).default;
+		const wordleWords = (await import("@/database/json/wordle_words.json")).default;
 
-        let trSum = 0;
-        let enSum = 0;
-        Object.values(turkishWords).forEach(
-            (wordList) => (trSum += wordList.length)
-        );
-        Object.values(englishWords).forEach(
-            (wordList) => (enSum += wordList.length)
-        );
+		let trSum = 0;
+		let enSum = 0;
+		Object.values(turkishWords).forEach((wordList) => (trSum += wordList.length));
+		Object.values(englishWords).forEach((wordList) => (enSum += wordList.length));
 
-        this.tr = turkishWords;
-        this.en = englishWords;
-        this.wordleWords = wordleWords;
+		this.tr = turkishWords;
+		this.en = englishWords;
+		this.wordleWords = wordleWords;
 
-        return {tr: trSum, en: enSum};
-    }
+		return { tr: trSum, en: enSum };
+	}
 }
