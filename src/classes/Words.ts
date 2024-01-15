400b3b8f72c70435eec8bcc14eb404fafbdf8ad6
kryparnold <emirhanpgt@gmail.com>
2024-01-15 21:26:35 +0300

    added save and method to add words and adjusted paths

--- src/classes/Words.ts
+++ src/classes/Words.ts
@@ -1,17 +1,19 @@
-import { IEnglishWords, ITurkishWords, IWordleWords } from "@/globals";
+import config from "@/config";
+import { FormattedLocale, IEnglishWords, ITurkishWords, IWordleWords, WordleLengths } from "@/globals";
 import { TextChannel } from "discord.js";
+import fs from "fs";
 
 export class Words {
 	tr!: ITurkishWords;
 	en!: IEnglishWords;
 	wordleWords!: IWordleWords;
-    wordReportChannel!: TextChannel;
+	wordReportChannel!: TextChannel;
 
 	async init(wordReportChannel: TextChannel) {
-        this.wordReportChannel = wordReportChannel;
-		const turkishWords = (await import("@/database/json/tr_words.json")).default;
-		const englishWords = (await import("@/database/json/en_words.json")).default;
-		const wordleWords = (await import("@/database/json/wordle_words.json")).default;
+		this.wordReportChannel = wordReportChannel;
+		const turkishWords = (await import(config.turkishWordsPath)).default as ITurkishWords;
+		const englishWords = (await import(config.englishWordsPath)).default as IEnglishWords;
+		const wordleWords = (await import(config.wordleWordsPath)).default as IWordleWords;
 
 		let trSum = 0;
 		let enSum = 0;
@@ -23,5 +25,22 @@ export class Words {
 		this.wordleWords = wordleWords;
 
 		return { tr: trSum, en: enSum };
+	}
+
+	async addWord(word: string, language: FormattedLocale) {
+		//@ts-ignore
+		this[language][word.at(0)].push(word);
+
+		if ([4, 5, 6].includes(word.length)) {
+			this.wordleWords[language][word.length as WordleLengths].push(word);
+		}
+
+		await this.save();
+	}
+
+	async save() {
+		await fs.promises.writeFile(config.turkishWordsPath, JSON.stringify(this.tr), { encoding: "utf-8" });
+		await fs.promises.writeFile(config.englishWordsPath, JSON.stringify(this.en), { encoding: "utf-8" });
+		await fs.promises.writeFile(config.wordleWordsPath, JSON.stringify(this.wordleWords), { encoding: "utf-8" });
 	}
 }
