1e4b253ff791b558c902e4616bc8095092be4116
kryparnold <emirhanpgt@gmail.com>
2024-01-15 20:25:00 +0300

    added delete button

---
+++ src/buttons/delete.ts
@@ -0,0 +1,10 @@
+import { ButtonInteraction } from "discord.js";
+
+export default {
+    data: {
+        id: "delete"
+    },
+    async execute(interaction: ButtonInteraction) {
+        await interaction.message.delete().catch(() => {});
+    }
+}
