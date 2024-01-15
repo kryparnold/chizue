038b34d823e5fa1b8bdf21057614ad1ecf328452
kryparnold <emirhanpgt@gmail.com>
2024-01-15 21:27:01 +0300

    added paths for json files

--- src/config.example.ts
+++ src/config.example.ts
@@ -1,4 +1,5 @@
 const srcPath = import.meta.dir;
+const jsonsPath = srcPath + "<JSON_FILES_PATH>"
 
 export default {
     guildId: "<GUILD_ID>",
@@ -8,9 +9,12 @@ export default {
     statsMessageChannelId: "<STATS_MESSAGE_CHANNEL_ID>",
     statsChannelId: "<STATS_CHANNEL_ID>",
     wordReportChannelId: "<WORD_REPORT_CHANNEL_ID>",
-    commandsPath: srcPath + "/<COMMANDS_FOLDER_PATH>",
-    buttonsPath: srcPath + "/<BUTTONS_FOLDER_PATH>",
-    statsPath: srcPath + "/<STATS_JSON_PATH>",
+    commandsPath: srcPath + "/<COMMANDS_FOLDER_NAME>",
+    buttonsPath: srcPath + "/<BUTTONS_FOLDER_NAME",
+    statsPath: jsonsPath + "/<STATS_JSON_PATH>",
+    englishWordsPath: jsonsPath + "/<ENGLISH_WORDS_JSON_FILENAME>",
+    turkishWordsPath: jsonsPath + "/<TURKISH_WORDS_JSON_FILENAME>",
+    wordleWordsPath: jsonsPath + "/<WORDLE_WORDS_JSON_FILENAME>",
     acceptEmote: "<ACCEPT_EMOTE_TAG_STRING>",
     denyEmote: "<DENY_EMOTE_TAG_STRING>"
 };
