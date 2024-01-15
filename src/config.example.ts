7723d18b8176ed3c8c41e109faed1032f6aaa68a
kryparnold <emirhanpgt@gmail.com>
2024-01-15 19:27:05 +0300

    added example config file

---
+++ src/config.example.ts
@@ -0,0 +1,15 @@
+const srcPath = import.meta.dir;
+
+export default {
+    guildId: "<GUILD_ID>",
+    logChannelId: "<LOG_CHANNEL_ID>",
+    clientId: "<CLIENT_ID>",
+    statsMessageId: "<STATS_MESSAGE_ID>",
+    statsMessageChannelId: "<STATS_MESSAGE_CHANNEL_ID>",
+    statsChannelId: "<STATS_CHANNEL_ID>",
+    commandsPath: srcPath + "/<COMMANDS_FOLDER_PATH>",
+    buttonsPath: srcPath + "/<BUTTONS_FOLDER_PATH>",
+    statsPath: srcPath + "/<STATS_JSON_PATH>",
+    acceptEmote: "<ACCEPT_EMOTE_TAG_STRING>",
+    denyEmote: "<DENY_EMOTE_TAG_STRING>"
+};
