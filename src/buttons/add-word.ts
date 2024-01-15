9cecbe614c81e933925ab4c098df358eeef62b4e
kryparnold <emirhanpgt@gmail.com>
2024-01-16 00:57:07 +0300

    adjusted response

--- src/buttons/add-word.ts
+++ src/buttons/add-word.ts
@@ -2,21 +2,19 @@ import { ButtonParams, FormattedLocale, client } from "@/globals";
 import { ButtonInteraction } from "discord.js";
 
 export default {
-    data: {
-        id: "add-word",
-        params: [
-            "word",
-            "language"
-        ]
-    },
-    async execute(interaction: ButtonInteraction, params: ButtonParams) {
-        const word = params["word"];
-        const language = params["language"] as FormattedLocale;
+	data: {
+		id: "add-word",
+		params: ["word", "language"],
+	},
+	async execute(interaction: ButtonInteraction, params: ButtonParams) {
+		const word = params["word"];
+		const language = params["language"] as FormattedLocale;
 
-        await client.words.addWord(word,language);
+		await client.words.addWord(word, language);
 
-        await interaction.reply({
-            content: `word: ${word}, language: ${language}`
-        });
-    }
-}
+		await interaction.reply({
+			content: `The word **${word}** added to ${client.getLocalization("en", language)} Words.`,
+            ephemeral: true
+		});
+	},
+};
