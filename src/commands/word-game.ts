e678065d7e8ab05487222d54a91bb83ab88e9385
kryparnold <emirhanpgt@gmail.com>
2024-01-18 01:38:42 +0300

    adjusted component collector methods

--- src/commands/word-game.ts
+++ src/commands/word-game.ts
@@ -139,175 +139,180 @@ export default {
             components: rows,
         });
 
-        // Create a message component collector for button and select menu interactions
-        const componentCollector = reply.createMessageComponentCollector({ time: 90000 });
+        // Set a flag to wait for a user interaction with a message component
+        let waitForComponent = true;
 
-        componentCollector
-            // Handle interactions within the collector
-            .on("collect", async (componentInteraction) => {
-                const customId = componentInteraction.customId;
-                await componentInteraction.deferReply({ ephemeral: true });
-                if (componentInteraction.isStringSelectMenu()) {
-                    // Handle interactions for string select menus
-                    const selectedOption: any = componentInteraction.values[0];
+        // Continue looping until a component interaction or a timeout occurs
+        while (waitForComponent) {
+            // Wait for a message component interaction with a timeout of 90 seconds
+            const componentInteraction = await reply.awaitMessageComponent({ time: 90000 });
 
-                    const options = componentInteraction.component.options;
+            // If no component interaction within the timeout, update reply and exit loop
+            if (!componentInteraction) {
+                waitForComponent = false;
 
-                    const localizedOption = options.find((option) => option.value === selectedOption)?.label as string;
+                // Update the reply with a timeout message
+                embed.setDescription(client.getLocalization<true>(userLocale, "commandTimeout")(this.data.name)).setFields([]);
+                await interaction.editReply({
+                    embeds: [embed],
+                    components: [],
+                });
+            }
 
-                    if (customId.endsWith("locale")) {
-                        // Handle locale change
-                        channelPreferences.locale = selectedOption;
-                        channel = await channel?.setLocale(selectedOption);
-                        await componentInteraction.editReply({
-                            content: client.getLocalization<true>(userLocale, "commandWordGameLangChange")(localizedOption),
-                        });
-                    } else if (customId.endsWith("mode")) {
-                        // Handle game mode change
-                        channelPreferences.mode = selectedOption;
-                        channel = await channel?.setMode(selectedOption);
-                        await componentInteraction.editReply({
-                            content: client.getLocalization<true>(userLocale, "commandWordGameModeChange")(localizedOption),
-                        });
-                    }
+            const customId = componentInteraction.customId;
+            await componentInteraction.deferReply({ ephemeral: true });
+            if (componentInteraction.isStringSelectMenu()) {
+                // Handle interactions for string select menus
+                const selectedOption: any = componentInteraction.values[0];
 
-                    // Update channel data in the database if it exists
-                    if (channel) {
-                        // Set default options for locale and mode select menus
-                        selectLocale.options.forEach((item) => item.setDefault(item.data.value === channel?.locale));
-                        selectMode.options.forEach((item) => item.setDefault(item.data.value === channel?.mode));
+                const options = componentInteraction.component.options;
 
-                        await interaction.editReply({
-                            embeds: [
-                                embed.setFields(
-                                    await this.getFields(userLocale, {
-                                        channelLocale: channel.locale,
-                                        letter: channel.letter,
-                                        mode: channel.mode,
-                                        wordCount: channel.words.length,
-                                    })
-                                ),
-                            ],
-                            components: rows,
-                        });
-                    }
-                } else if (componentInteraction.isButton()) {
-                    // Handle interactions for buttons
-                    if (customId.endsWith("setup")) {
-                        // Handle setup button click
-                        const randomLetter = Utils.randomLetter(Utils.formatLocale(channelPreferences.locale));
-                        const randomWords = channelPreferences.locale === Locales.English ? Utils.getRandomWords(3) : [];
+                const localizedOption = options.find((option) => option.value === selectedOption)?.label as string;
 
-                        channel = await client.games.createWordGame({
-                            id: selectedChannel.id,
-                            letter: randomLetter,
-                            locale: channelPreferences.locale,
-                            guildId: interaction.guildId as string,
-                            mode: channelPreferences.mode,
-                            randomWords,
-                        });
+                if (customId.endsWith("locale")) {
+                    // Handle locale change
+                    channelPreferences.locale = selectedOption;
+                    channel = await channel?.setLocale(selectedOption);
+                    await componentInteraction.editReply({
+                        content: client.getLocalization<true>(userLocale, "commandWordGameLangChange")(localizedOption),
+                    });
+                } else if (customId.endsWith("mode")) {
+                    // Handle game mode change
+                    channelPreferences.mode = selectedOption;
+                    channel = await channel?.setMode(selectedOption);
+                    await componentInteraction.editReply({
+                        content: client.getLocalization<true>(userLocale, "commandWordGameModeChange")(localizedOption),
+                    });
+                }
 
-                        embed
-                            .setDescription(client.getLocalization<true>(userLocale, "commandGameExists")(client.getLocalization(userLocale, channel.type)))
-                            .addFields(
+                // Update channel data in the database if it exists
+                if (channel) {
+                    // Set default options for locale and mode select menus
+                    selectLocale.options.forEach((item) => item.setDefault(item.data.value === channel?.locale));
+                    selectMode.options.forEach((item) => item.setDefault(item.data.value === channel?.mode));
+
+                    await interaction.editReply({
+                        embeds: [
+                            embed.setFields(
                                 await this.getFields(userLocale, {
                                     channelLocale: channel.locale,
                                     letter: channel.letter,
                                     mode: channel.mode,
                                     wordCount: channel.words.length,
                                 })
-                            );
-
-                        // Set default options for locale and mode select menus
-                        selectLocale.options.forEach((item) => item.setDefault(item.data.value === channel?.locale));
-                        selectMode.options.forEach((item) => item.setDefault(item.data.value === channel?.mode));
-
-                        await interaction.editReply({
-                            embeds: [embed],
-                            components: rows.map((item, index) => {
-                                if (index === rows.length - 1) {
-                                    item.components[0].setDisabled(true);
-                                    item.components[1].setDisabled(false);
-                                }
-
-                                return item;
-                            }),
-                        });
-                        await componentInteraction.editReply({
-                            content: client.getLocalization(userLocale, "commandGameSetupSuccess"),
-                        });
-                    } else if (customId.endsWith("remove")) {
-                        // Handle remove button click
-                        const confirmEmbed = new EmbedBuilder()
-                            .setTitle(client.getLocalization(userLocale, "confirmRemove"))
-                            .setDescription(client.getLocalization(userLocale, "confirmRemoveDesc"))
-                            .setColor(Colors.Red)
-                            .setFooter(userFooter);
-
-                        const confirmButton = new ButtonBuilder()
-                            .setCustomId("_confirm")
-                            .setLabel(client.getLocalization(userLocale, "buttonConfirm"))
-                            .setStyle(ButtonStyle.Primary);
-
-                        const row = new ActionRowBuilder<ButtonBuilder>().addComponents(confirmButton);
-
-                        // Display a confirmation message and button
-                        const componentReply = await componentInteraction.editReply({
-                            embeds: [confirmEmbed],
-                            components: [row],
-                        });
-                        // Await user confirmation or timeout
-                        componentReply
-                            .awaitMessageComponent({ time: 15000, componentType: ComponentType.Button })
-                            .then(async (buttonInteraction) => {
-                                await buttonInteraction.deferReply({ ephemeral: true });
-
-                                // Delete the Word Game from the database upon confirmation
-                                await client.games.delete(channel?.id);
-                                channel = undefined;
-
-                                embed.data.description = client.getLocalization(userLocale, "commandGameDoesntExists");
-                                embed.data.fields = [];
-
-                                interaction.editReply({
-                                    embeds: [embed],
-                                    components: rows.map((item, index) => {
-                                        if (index === rows.length - 1) {
-                                            item.components[1].setDisabled(true);
-                                            item.components[0].setDisabled(false);
-                                        }
-
-                                        return item;
-                                    }),
-                                });
-
-                                buttonInteraction.editReply({
-                                    content: client.getLocalization(userLocale, "commandGameRemoveSuccess"),
-                                });
-
-                                componentInteraction.editReply({
-                                    embeds: [confirmEmbed.setDescription(client.getLocalization(userLocale, "confirmUsed"))],
-                                    components: [],
-                                });
-                            })
-                            .catch(() => {
-                                componentInteraction.editReply({
-                                    embeds: [confirmEmbed.setDescription(client.getLocalization(userLocale, "confirmRemoveExpire"))],
-                                    components: [],
-                                });
-                            });
-                    }
+                            ),
+                        ],
+                        components: rows,
+                    });
                 }
-            })
+            } else if (componentInteraction.isButton()) {
+                // Handle interactions for buttons
+                if (customId.endsWith("setup")) {
+                    // Handle setup button click
+                    const randomLetter = Utils.randomLetter(Utils.formatLocale(channelPreferences.locale));
+                    const randomWords = channelPreferences.locale === Locales.English ? Utils.getRandomWords(3) : [];
 
-            // Handle the end of the collector by updating the embed and components
-            .on("end", async () => {
-                embed.setDescription(client.getLocalization<true>(userLocale, "commandTimeout")(this.data.name)).setFields([]);
-                await interaction.editReply({
-                    embeds: [embed],
-                    components: [],
-                });
-            });
+                    channel = await client.games.createWordGame({
+                        id: selectedChannel.id,
+                        letter: randomLetter,
+                        locale: channelPreferences.locale,
+                        guildId: interaction.guildId as string,
+                        mode: channelPreferences.mode,
+                        randomWords,
+                    });
+
+                    embed
+                        .setDescription(client.getLocalization<true>(userLocale, "commandGameExists")(client.getLocalization(userLocale, channel.type)))
+                        .addFields(
+                            await this.getFields(userLocale, {
+                                channelLocale: channel.locale,
+                                letter: channel.letter,
+                                mode: channel.mode,
+                                wordCount: channel.words.length,
+                            })
+                        );
+
+                    // Set default options for locale and mode select menus
+                    selectLocale.options.forEach((item) => item.setDefault(item.data.value === channel?.locale));
+                    selectMode.options.forEach((item) => item.setDefault(item.data.value === channel?.mode));
+
+                    await interaction.editReply({
+                        embeds: [embed],
+                        components: rows.map((item, index) => {
+                            if (index === rows.length - 1) {
+                                item.components[0].setDisabled(true);
+                                item.components[1].setDisabled(false);
+                            }
+
+                            return item;
+                        }),
+                    });
+                    await componentInteraction.editReply({
+                        content: client.getLocalization(userLocale, "commandGameSetupSuccess"),
+                    });
+                } else if (customId.endsWith("remove")) {
+                    // Handle remove button click
+                    const confirmEmbed = new EmbedBuilder()
+                        .setTitle(client.getLocalization(userLocale, "confirmRemove"))
+                        .setDescription(client.getLocalization(userLocale, "confirmRemoveDesc"))
+                        .setColor(Colors.Red)
+                        .setFooter(userFooter);
+
+                    const confirmButton = new ButtonBuilder()
+                        .setCustomId("_confirm")
+                        .setLabel(client.getLocalization(userLocale, "buttonConfirm"))
+                        .setStyle(ButtonStyle.Primary);
+
+                    const row = new ActionRowBuilder<ButtonBuilder>().addComponents(confirmButton);
+
+                    // Display a confirmation message and button
+                    const componentReply = await componentInteraction.editReply({
+                        embeds: [confirmEmbed],
+                        components: [row],
+                    });
+                    // Await user confirmation or timeout
+                    componentReply
+                        .awaitMessageComponent({ time: 15000, componentType: ComponentType.Button })
+                        .then(async (buttonInteraction) => {
+                            await buttonInteraction.deferReply({ ephemeral: true });
+
+                            // Delete the Word Game from the database upon confirmation
+                            await client.games.delete(channel?.id);
+                            channel = undefined;
+
+                            embed.data.description = client.getLocalization(userLocale, "commandGameDoesntExists");
+                            embed.data.fields = [];
+
+                            interaction.editReply({
+                                embeds: [embed],
+                                components: rows.map((item, index) => {
+                                    if (index === rows.length - 1) {
+                                        item.components[1].setDisabled(true);
+                                        item.components[0].setDisabled(false);
+                                    }
+
+                                    return item;
+                                }),
+                            });
+
+                            buttonInteraction.editReply({
+                                content: client.getLocalization(userLocale, "commandGameRemoveSuccess"),
+                            });
+
+                            componentInteraction.editReply({
+                                embeds: [confirmEmbed.setDescription(client.getLocalization(userLocale, "confirmUsed"))],
+                                components: [],
+                            });
+                        })
+                        .catch(() => {
+                            componentInteraction.editReply({
+                                embeds: [confirmEmbed.setDescription(client.getLocalization(userLocale, "confirmRemoveExpire"))],
+                                components: [],
+                            });
+                        });
+                }
+            }
+        }
     },
     // Helper function to generate fields for the embed
     async getFields(locale: any, props: { wordCount: number; letter: string; channelLocale: Locales; mode: GameMode }) {
