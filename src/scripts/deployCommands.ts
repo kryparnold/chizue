2f2473e620f19f406906b9a679032f21560670d2
kryparnold <emirhanpgt@gmail.com>
2024-02-07 16:42:58 +0300

    fixed guild command adding

--- src/scripts/deployCommands.ts
+++ src/scripts/deployCommands.ts
@@ -15,15 +15,18 @@ const commandFiles = readdirSync(config.commandsPath);
 for (const file of commandFiles) {
     const filePath = path.join(config.commandsPath, file);
     const command: any = (await import(filePath)).default;
-    for (let i = 0; i < command.guildIds.length; i++) {
-        const guildId = command.guildIds[i];
+    if (command.guildIds) {
+        for (let i = 0; i < command.guildIds.length; i++) {
+            const guildId = command.guildIds[i];
 
-        if (!guildCommands[guildId]) {
-            guildCommands[guildId] = { id: guildId, commands: [] };
+            if (!guildCommands[guildId]) {
+                guildCommands[guildId] = { id: guildId, commands: [] };
+            }
+            guildCommands[guildId].commands.push(command.data.toJSON());
         }
-        guildCommands[guildId].commands.push(command.data.toJSON());
+    } else {
+        commands.push(command.data.toJSON());
     }
-    if (!command.guildIds) commands.push(command.data.toJSON());
 }
 
 const rest = new REST().setToken(process.env.TOKEN!);
